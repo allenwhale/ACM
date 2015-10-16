34c6fbd6f4720ece548753aaf2b253bdd55a87d1
Tocknicsu <wingemerald@gmail.com>
2015-10-16 16:15:04 +0800

    update

--- java/test.java
+++ java/test.java
@@ -4,17 +4,23 @@ public class test {
     public static void main(String[] args) {
         String s;
         Scanner scanner = new Scanner(System.in);
+        // Scanner: next() nextInt() nextLine() nextDouble()
         int n = scanner.nextInt();
-        BigInteger[] b = new BigInteger[100];
-        for(int i = 0 ; i < n ; i++){
-            b[i] = new BigInteger(scanner.next());
-            System.out.println(b[i]);
-        }
+        BigInteger[] b = new BigInteger[2];
+        b[0] = new BigInteger(scanner.next());
+        b[1] = new BigInteger(scanner.next());
         BigInteger ans = new BigInteger("0");
-        for(int i = 0 ; i < n ; i++){
-            ans = ans.add(b[i]);
-            //add subtract multiply divide mod isProbablePrime gcd
-            System.out.println(b[i]+" "+ans);
+        //sort:
+        Arrays.sort(b);
+        Arrays.sort(b, new comp());
+        ans = b[0].add(b[1]);
+        //BigInteger: add subtract multiply divide mod isProbablePrime gcd
+        System.out.println(b[i]+" "+ans);
+        //Output: print println
+    }
+    static class comp implements Comparator<Integer> {
+        public int compare(Integer i1, Integer i2) {
+            return i2 > i1;
         }
     }
 }
